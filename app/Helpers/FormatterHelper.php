e8e40445b46dcbd68662275483a5c71e3c26ce95
maxwellmezadre <maxwellmezadre@gmail.com>
2023-06-04 08:49:16 -0300

    Criado helper para formatação.

---
+++ app/Helpers/FormatterHelper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helpers;
+
+abstract class FormatterHelper
+{
+    public static function moneyToDecimal(mixed $money): float
+    {
+        $cleanString = preg_replace('/([^0-9\.,])/i', '', $money);
+        $onlyNumbersString = preg_replace('/([^0-9])/i', '', $money);
+
+        $separatorsCountToBeErased = strlen($cleanString) - strlen($onlyNumbersString) - 1;
+
+        $stringWithCommaOrDot = preg_replace('/([,\.])/', '', $cleanString, $separatorsCountToBeErased);
+        $removedThousandSeparator = preg_replace('/(\.|,)(?=[0-9]{3,}$)/', '',  $stringWithCommaOrDot);
+
+        return (float) str_replace(',', '.', $removedThousandSeparator);
+    }
+}
