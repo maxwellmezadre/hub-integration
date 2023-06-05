91784425a7954dfbc0f1708f6b51d3b1728c32ae
maxwellmezadre <maxwellmezadre@gmail.com>
2023-06-04 23:25:50 -0300

    Adicionadas rotas de listagem de produtos, variaçãoes e a rota de integração.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Services\ERP\ConfectionERPService;
+use App\Integration\SalesPlatform\IntegrationHub;
+use App\Transformers\SalesPlatform\ProductsConfectionERPTransformer;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/produtos', function () {
+    $service = new ConfectionERPService();
+    $products = $service->getProducts();
+
+    dump($products);
+});
+
+Route::get('/variacoes', function () {
+    $service = new ConfectionERPService();
+    $variations = $service->getVariations();
+
+    dump($variations);
+});
+
+Route::get('/integracao', function () {
+    $integrationHub = new IntegrationHub(
+        new ConfectionERPService(),
+        new ProductsConfectionERPTransformer()
+    );
+
+    try {
+        if ($integrationHub->syncProducts()) {
+            echo 'Products synced successfully!';
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
 });
